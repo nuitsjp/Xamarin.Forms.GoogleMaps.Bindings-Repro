21039a96ed6477fe3429cbed7a2e0f9bd6842b8c
nuits.jp <nuits.jp@live.jp>
2018-07-19 01:28:28 +0900

    Add MapView and other.

--- Xamarin.Forms.GoogleMaps.Bindings Repro/App1/App1/App1/MainPage.xaml.cs
+++ Xamarin.Forms.GoogleMaps.Bindings Repro/App1/App1/App1/MainPage.xaml.cs
@@ -1,17 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Forms.GoogleMaps;
+using Xamarin.Forms.GoogleMaps.Bindings;
 
 namespace App1
 {
 	public partial class MainPage : ContentPage
 	{
-		public MainPage()
-		{
-			InitializeComponent();
-		}
-	}
+        public ObservableCollection<Pin> Pins { get; set; }
+
+        public MoveToRegionRequest Request { get; } = new MoveToRegionRequest();
+
+
+        public Command MoveCommand => new Command(() =>
+        {
+            Request.MoveToRegion(
+                MapSpan.FromCenterAndRadius(
+                    new Position(49.628222, 6.164697),
+                    Distance.FromKilometers(2)),
+                true);
+            Pins.Add(new Pin()
+            {
+                Label = "Test1",
+                Address = "1, rue Lala",
+                Position = new Position(49.628222, 6.164697)
+            });
+        });
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
 }
